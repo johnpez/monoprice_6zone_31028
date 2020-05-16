24a6de5e4c450ebc093035dcdc81de17a922cceb
John Pezzetti <john@johnpezzetti.com>
2020-05-16 09:14:24 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,60 @@
+"""The Monoprice 6-Zone (31028) Amplifier integration."""
+import asyncio
+import logging
+
+from pymonoprice_6zone_31028 import get_monoprice
+from serial import SerialException
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_PORT
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .const import DOMAIN
+
+PLATFORMS = ["media_player"]
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up the Monoprice 6-Zone Amplifier 31028 component."""
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Monoprice 6-Zone Amplifier from a config entry."""
+    port = entry.data[CONF_PORT]
+
+    try:
+        monoprice = await hass.async_add_executor_job(get_monoprice, port)
+        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = monoprice
+    except SerialException:
+        _LOGGER.error("Error connecting to Monoprice controller at %s", port)
+        raise ConfigEntryNotReady
+
+    entry.add_update_listener(_update_listener)
+
+    for component in PLATFORMS:
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, component)
+        )
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[
+                hass.config_entries.async_forward_entry_unload(entry, component)
+                for component in PLATFORMS
+            ]
+        )
+    )
+
+    return unload_ok
+
+
+async def _update_listener(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
---
+++ const.py
@@ -0,0 +1,11 @@
+"""Constants for the Monoprice 6-Zone (31028) Amplifier Media Player component."""
+
+DOMAIN = "monoprice_6zone_31028"
+
+CONF_SOURCES = "sources"
+
+CONF_SOURCE_1 = "source_1"
+CONF_SOURCE_2 = "source_2"
+
+SERVICE_SNAPSHOT = "snapshot"
+SERVICE_RESTORE = "restore"
